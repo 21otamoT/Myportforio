0cdf1e6c9a4bbc69c4f70355596cf054d998bd9d
hirose <yukihio76@gmail.com>
2023-11-25 00:57:14 +0900

    シャボオン玉の実装

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
 $(function () {
+  const box = document.getElementById('cercle');
+  const boxWidth = box.offsetWidth;
+  const boxHeight = box.offsetHeight;
 
+  for(let i =0; i < 20; i++) {
+    // ボックスを作る
+    const clieitBox = () => {
+      const newBox = document.createElement('p');
+      box.appendChild(newBox);
+    };
+
+    // ランダムな位置に配置する
+    const coordinates = () => {
+      const pArray = document.querySelectorAll('#cercle p');
+
+        pArray.forEach(p => {
+          let randumWidth = Math.random() * boxWidth;
+          let randumHeight = Math.random() * boxHeight;
+          p.style.top = randumWidth+'px';
+          p.style.left = randumHeight+'px';
+
+          // ボックスにアニメーションをつける
+          const boxMove = [
+            {transform: 'translate(20px, 0)'},
+            {transform: 'translate(-20px,0)'},
+          ];
+
+          p.animate(boxMove,{
+            duration: 3000,
+            direction:"alternate",
+            iterations: Infinity,
+            
+          });
+          console.log(`'pWidth'${randumWidth}+'pHeight${randumHeight}`);
+        });                         
+    };
+      clieitBox();
+      coordinates();
+}
   //ページ内スクロール
   var navHeight = $(".header").outerHeight();
 
@@ -40,6 +78,9 @@ $(function () {
       const position = $(this).offset().top;
       if (scrollAmount > position - windowHeight + 100) {
         $(this).addClass('fade-in');
+      }
+      else {
+        $(this).removeClass('fade-in');
       }
     });
   });
