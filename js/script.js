32a47932ed23c7a92942f39addbda6fdfe120420
hirose <yukihio76@gmail.com>
2023-11-29 16:09:55 +0900

    カルーセルのエラーの修正

--- js/script.js
+++ js/script.js
@@ -60,42 +60,4 @@ $(function () {
     $('.openbtn1').toggleClass('active');
     $('#g-nav').toggleClass('active');
   });
-
-  // カルーセル
-  $('.carousel').slick({
-    autoplay: true,
-    dots: true,
-    infinite: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-  });
-
-  // スクロールしたときにセクションをフェードインさせる
-  $(window).scroll(function () {
-    const scrollAmount = $(window).scrollTop();
-    const windowHeight = $(window).height();
-    $('.jquery').each(function () {
-      const position = $(this).offset().top;
-      if (scrollAmount > position - windowHeight + 100) {
-        $(this).addClass('fade-in');
-      }
-      else {
-        $(this).removeClass('fade-in');
-      }
-    });
-  });
-
-  // Worksの画像をクリックしたときにモーダルで拡大表示する
-  $('.md img').click(function () {
-    const imgSrc = $(this).attr('src');
-    $('.big-img').attr('src', imgSrc);
-    $('.modal').fadeIn();
-    return false
-  });
-
-  // 閉じるボタンをクリックしたときにモーダルを閉じる
-  $('.close-btn').click(function () {
-    $('.modal').fadeOut();
-    return false
-  });
 });
---
+++ js/sub.js
@@ -0,0 +1,39 @@
+$(function(){
+  // カルーセル
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
+
+  // スクロールしたときにセクションをフェードインさせる
+  $(window).scroll(function () {
+    const scrollAmount = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    $('.jquery').each(function () {
+      const position = $(this).offset().top;
+      if (scrollAmount > position - windowHeight + 100) {
+        $(this).addClass('fade-in');
+      }
+      else {
+        $(this).removeClass('fade-in');
+      }
+    });
+  });
+
+  // Worksの画像をクリックしたときにモーダルで拡大表示する
+  $('.md img').click(function () {
+    const imgSrc = $(this).attr('src');
+    $('.big-img').attr('src', imgSrc);
+    $('.modal').fadeIn();
+    return false
+  });
+
+  // 閉じるボタンをクリックしたときにモーダルを閉じる
+  $('.close-btn').click(function () {
+    $('.modal').fadeOut();
+    return false
+  });
+});
